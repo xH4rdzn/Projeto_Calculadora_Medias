a741c7763d05009d6771a15e20328f1445574444
Igor Coelho <xh4rdzn@gmail.com>
2022-12-04 15:29:03 -0300

    Fixed Value Average  Final

--- main.js
+++ main.js
@@ -46,7 +46,8 @@ function atualizaTabela() {
 
 function atualizaMediaFinal() {
   const mediaFinal = calculaMediaFinal();
-  document.getElementById("media-final-valor").innerHTML = mediaFinal;
+  document.getElementById("media-final-valor").innerHTML =
+    mediaFinal.toFixed(2);
   document.getElementById("media-final-resultado").innerHTML =
     mediaFinal >= notaMin ? spanAprovado : spanReprovado;
 }
